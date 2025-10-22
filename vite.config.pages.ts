0a6c6110296317e561e79aea6adb7931b8122ba0
albertchouforces <albert.chou.forces@gmail.com>
2025-10-22 11:27:48 -0700

    dir fix

--- vite.config.pages.ts
+++ vite.config.pages.ts
@@ -1,18 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { cloudflare } from "@cloudflare/vite-plugin";
+import { mochaPlugins } from "@getmocha/vite-plugins";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  plugins: [...mochaPlugins(process.env as any), react(), cloudflare()],
+  server: {
+    allowedHosts: true,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
   },
   build: {
+    chunkSizeWarningLimit: 5000,
     outDir: '.',
-    emptyOutDir: true,
+    emptyOutDir: false,
     sourcemap: false,
     minify: 'esbuild',
     rollupOptions: {
