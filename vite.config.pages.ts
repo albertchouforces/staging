820e4c40ec3f30fa26108a172701a8454bdebe29
albertchouforces <albert.chou.forces@gmail.com>
2025-10-22 13:29:48 -0700

    Update vite.config.pages.ts

--- vite.config.pages.ts
+++ vite.config.pages.ts
@@ -1,12 +1,9 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { cloudflare } from "@cloudflare/vite-plugin";
-import { mochaPlugins } from "@getmocha/vite-plugins";
 
 export default defineConfig({
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  plugins: [...mochaPlugins(process.env as any), react(), cloudflare()],
+  plugins: [react()],
   server: {
     allowedHosts: true,
   },
@@ -14,7 +11,11 @@ export default defineConfig({
     chunkSizeWarningLimit: 5000,
     outDir: "dist",
     emptyOutDir: true,
+    rollupOptions: {
+      input: "index.html"
+    }
   },
+  publicDir: "public",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
